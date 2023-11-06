cf8b2d402df20a4fe5013efaef9c19fe2b07d6e0
Bruce NKUNDABAGENZI <nbruce420@gmail.com>
2023-11-06 22:20:43 +0200

    finished map

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -27,6 +27,7 @@ const Map: React.FC<MapProps> = ({ center }) => {
             className="h-[45vh] rounded-lg"
         >
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            {center && <Marker position={center as L.LatLngExpression} />}
         </MapContainer>
     );
 };
