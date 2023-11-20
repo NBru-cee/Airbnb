f3f3f0ed05e6e848791626a62ce5c24b56889160
Bruce NKUNDABAGENZI <nbruce420@gmail.com>
2023-11-20 14:50:16 +0200

    added favs

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
-import Image from "next/image";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import getListings from "@/app/actions/getListings";
+import ListingCard from "@/app/components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
-export default function Home() {
-    const isEmpty = true;
-    if (!isEmpty) {
+export default async function Home() {
+    const listings = await getListings();
+    const currentUser = await getCurrentUser();
+
+    if (listings.length === 0) {
         return (
             <ClientOnly>
-                <EmptyState />
+                <EmptyState showReset />
             </ClientOnly>
         );
     }
@@ -17,7 +21,13 @@ export default function Home() {
         <ClientOnly>
             <Container>
                 <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:gric-cols-6 gap-8">
-                    <div>My future listings</div>
+                    {listings.map((listing) => (
+                        <ListingCard
+                            key={listing.id}
+                            data={listing}
+                            currentUser={currentUser}
+                        />
+                    ))}
                 </div>
             </Container>
         </ClientOnly>
