6e1c4727285a29e21a998b04bdb47d9834305641
Bruce NKUNDABAGENZI <nbruce420@gmail.com>
2023-11-23 11:21:36 +0200

    changed config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        serverActions: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     images: {
         remotePatterns: [
             {
+                protocol: "https",
                 hostname: "avatars.githubusercontent.com",
             },
             {
+                protocol: "https",
                 hostname: "res.cloudinary.com",
             },
             {
+                protocol: "https",
                 hostname: "lh3.googleusercontent.com",
             },
         ],
